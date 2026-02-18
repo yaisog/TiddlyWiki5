5813541dc458978ec0f8f6d8b3ea57591fa938a3
yaisog <m@rcuswinter.de>
2026-02-18 19:48:51 +0100

    Update to ES2020

--- core/modules/filters/get.js
+++ core/modules/filters/get.js
@@ -13,23 +13,23 @@ Filter operator for replacing tiddler titles by the value of the field specified
 Export our filter function
 */
 exports.get = function(source,operator,options) {
-	options = options || {};
-	var results = [],
-		wiki = options.wiki || $tw.wiki;
+	const results = [];
+	const fieldName = operator.operand;
 	source(function(tiddler,title) {
-		if(tiddler) {
-			var value;
-			if(tiddler.hasField("_is_skinny") && operator.operand === "text" && wiki) {
-				value = wiki.getTiddlerText(title,"");
-				if(value === null) {
-					return;
-				}
-			} else {
-				value = tiddler.getFieldString(operator.operand);
-			}
-			if(value) {
-				results.push(value);
-			}
+		if(!tiddler) return;
+		
+		let value;
+		// Check if we need to handle lazy loading for skinny tiddlers
+		if(fieldName === "text" && tiddler.hasField("_is_skinny")) {
+			const wiki = options?.wiki ?? $tw.wiki;
+			value = wiki.getTiddlerText(title,"");
+			// Skip this tiddler if it's being lazily loaded (returns null)
+			if(value === null) return;
+		} else {
+			value = tiddler.getFieldString(fieldName);
+		}
+		if(value) {
+			results.push(value);
 		}
 	});
 	return results;
