6262e78c5872a058c299c1344a69cac9cb9a2cfa
yaisog <m@rcuswinter.de>
2026-02-18 19:42:01 +0100

    Add skinny tiddler handling

--- core/modules/filters/get.js
+++ core/modules/filters/get.js
@@ -13,10 +13,20 @@ Filter operator for replacing tiddler titles by the value of the field specified
 Export our filter function
 */
 exports.get = function(source,operator,options) {
-	var results = [];
+	options = options || {};
+	var results = [],
+		wiki = options.wiki || $tw.wiki;
 	source(function(tiddler,title) {
 		if(tiddler) {
-			var value = tiddler.getFieldString(operator.operand);
+			var value;
+			if(tiddler.hasField("_is_skinny") && operator.operand === "text" && wiki) {
+				value = wiki.getTiddlerText(title,"");
+				if(value === null) {
+					return;
+				}
+			} else {
+				value = tiddler.getFieldString(operator.operand);
+			}
 			if(value) {
 				results.push(value);
 			}
